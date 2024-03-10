765055cedc9cfd6c1aad2cf9494fcc1b8f440d25
VasiliiP <gollandec@yandex.ru>
2024-03-10 13:36:06 +0100

    Added cache

--- src/ReportService.Infrastructure/Config/HrApiOptions.cs
+++ src/ReportService.Infrastructure/Config/HrApiOptions.cs
@@ -3,4 +3,5 @@ namespace ReportService.Infrastructure.Config;
 public class HrApiOptions
 {
     public string BaseAddress { get; set; } = null!;
+    public int CacheTimeToLiveInHours { get; set; }
 }
