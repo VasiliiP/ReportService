4981b6161692bee8de0433e13e83b42626b0b35b
vasiliy.polyanin <gollandec@yandex.ru>
2024-03-11 10:00:13 +0100

    Added docker support

--- src/ReportService.Web/Program.cs
+++ src/ReportService.Web/Program.cs
@@ -36,11 +36,11 @@ builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 var app = builder.Build();
 app.UseExceptionHandler(opt => { });
 
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
